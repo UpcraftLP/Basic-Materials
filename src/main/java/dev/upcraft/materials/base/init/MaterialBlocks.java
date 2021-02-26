ec04f879953fea6d6b26939487e2167572348fdf
Joshua Schirra <j-schirra@gmx.de>
2021-02-26 14:38:39 +0100

    require pickaxes for mining blocks

--- src/main/java/dev/upcraft/materials/base/init/MaterialBlocks.java
+++ src/main/java/dev/upcraft/materials/base/init/MaterialBlocks.java
@@ -30,6 +30,7 @@ import dev.upcraft.materials.base.gen.MaterialWorldgenFeatures;
 import io.github.glasspane.mesh.api.annotation.AutoRegistry;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.block.MaterialColor;
@@ -39,31 +40,31 @@ import net.minecraft.world.biome.Biome;
 @AutoRegistry.Register(value = Block.class, modid = BasicMaterials.MODID, registry = "block")
 public class MaterialBlocks {
 
-    //ore blocks
-    public static final Block BAUXITE_ORE = new MaterialOreBlock(Block.Settings.of(Material.STONE).strength(3.0F, 3.0F)).withDefaultConfig(); //Aluminum
-    public static final Block CINNABAR_ORE = new MaterialOreBlock(Block.Settings.of(Material.STONE).strength(3.0F, 3.0F), 0, 2).withConfig(new MaterialsOreConfig.GenData(0, 0, 100, 8, 0, false, BiomeSelectors.categories(Biome.Category.DESERT), MaterialWorldgenFeatures.RULE_SANDSTONE)); //Mercury
-    public static final Block COPPER_ORE = new MaterialOreBlock(Block.Settings.of(Material.STONE).strength(3.0F, 3.0F)).withDefaultConfig();
-    public static final Block LEAD_ORE = new MaterialOreBlock(Block.Settings.of(Material.STONE).strength(3.0F, 3.0F)).withDefaultConfig();
-    public static final Block NICKEL_ORE = new MaterialOreBlock(Block.Settings.of(Material.STONE).strength(3.0F, 3.0F)).withDefaultConfig();
-    public static final Block PLATINUM_ORE = new MaterialOreBlock(Block.Settings.of(Material.STONE).strength(3.0F, 3.0F)).withDefaultConfig();
-    public static final Block SILVER_ORE = new MaterialOreBlock(Block.Settings.of(Material.STONE).strength(3.0F, 3.0F)).withConfig(new MaterialsOreConfig.GenData(32, 0, 64, 6, 0));
-    public static final Block TIN_ORE = new MaterialOreBlock(Block.Settings.of(Material.STONE).strength(3.0F, 3.0F)).withDefaultConfig();
-    public static final Block ZINC_ORE = new MaterialOreBlock(Block.Settings.of(Material.STONE).strength(3.0F, 3.0F)).withDefaultConfig();
-
     //TODO material colors; different strengths
+    //ore blocks
+    public static final Block BAUXITE_ORE = new MaterialOreBlock(FabricBlockSettings.of(Material.STONE, MaterialColor.STONE).strength(3.0F, 3.0F).sounds(BlockSoundGroup.STONE).breakByTool(FabricToolTags.PICKAXES)).withDefaultConfig(); //Aluminum
+    public static final Block CINNABAR_ORE = new MaterialOreBlock(FabricBlockSettings.of(Material.STONE, MaterialColor.STONE).strength(3.0F, 3.0F).sounds(BlockSoundGroup.STONE).breakByTool(FabricToolTags.PICKAXES), 0, 2).withConfig(new MaterialsOreConfig.GenData(0, 0, 100, 8, 0, false, BiomeSelectors.categories(Biome.Category.DESERT), MaterialWorldgenFeatures.RULE_SANDSTONE)); //Mercury
+    public static final Block COPPER_ORE = new MaterialOreBlock(FabricBlockSettings.of(Material.STONE, MaterialColor.STONE).strength(3.0F, 3.0F).sounds(BlockSoundGroup.STONE).breakByTool(FabricToolTags.PICKAXES)).withDefaultConfig();
+    public static final Block LEAD_ORE = new MaterialOreBlock(FabricBlockSettings.of(Material.STONE, MaterialColor.STONE).strength(3.0F, 3.0F).sounds(BlockSoundGroup.STONE).breakByTool(FabricToolTags.PICKAXES)).withDefaultConfig();
+    public static final Block NICKEL_ORE = new MaterialOreBlock(FabricBlockSettings.of(Material.STONE, MaterialColor.STONE).strength(3.0F, 3.0F).sounds(BlockSoundGroup.STONE).breakByTool(FabricToolTags.PICKAXES)).withDefaultConfig();
+    public static final Block PLATINUM_ORE = new MaterialOreBlock(FabricBlockSettings.of(Material.STONE, MaterialColor.STONE).strength(3.0F, 3.0F).sounds(BlockSoundGroup.STONE).breakByTool(FabricToolTags.PICKAXES)).withDefaultConfig();
+    public static final Block SILVER_ORE = new MaterialOreBlock(FabricBlockSettings.of(Material.STONE, MaterialColor.STONE).strength(3.0F, 3.0F).sounds(BlockSoundGroup.STONE).breakByTool(FabricToolTags.PICKAXES)).withConfig(new MaterialsOreConfig.GenData(32, 0, 64, 6, 0));
+    public static final Block TIN_ORE = new MaterialOreBlock(FabricBlockSettings.of(Material.STONE, MaterialColor.STONE).strength(3.0F, 3.0F).sounds(BlockSoundGroup.STONE).breakByTool(FabricToolTags.PICKAXES)).withDefaultConfig();
+    public static final Block ZINC_ORE = new MaterialOreBlock(FabricBlockSettings.of(Material.STONE, MaterialColor.STONE).strength(3.0F, 3.0F).sounds(BlockSoundGroup.STONE).breakByTool(FabricToolTags.PICKAXES)).withDefaultConfig();
+
     //storage blocks
-    public static final Block ALUMINUM_BLOCK = new MaterialBlock(FabricBlockSettings.of(Material.METAL, MaterialColor.IRON).strength(5.0F, 6.0F).sounds(BlockSoundGroup.METAL));
-    public static final Block COPPER_BLOCK = new MaterialBlock(FabricBlockSettings.of(Material.METAL, MaterialColor.IRON).strength(5.0F, 6.0F).sounds(BlockSoundGroup.METAL));
-    public static final Block LEAD_BLOCK = new MaterialBlock(FabricBlockSettings.of(Material.METAL, MaterialColor.IRON).strength(5.0F, 6.0F).sounds(BlockSoundGroup.METAL));
-    public static final Block NICKEL_BLOCK = new MaterialBlock(FabricBlockSettings.of(Material.METAL, MaterialColor.IRON).strength(5.0F, 6.0F).sounds(BlockSoundGroup.METAL));
-    public static final Block PLATINUM_BLOCK = new MaterialBlock(FabricBlockSettings.of(Material.METAL, MaterialColor.IRON).strength(5.0F, 6.0F).sounds(BlockSoundGroup.METAL));
-    public static final Block SILVER_BLOCK = new MaterialBlock(FabricBlockSettings.of(Material.METAL, MaterialColor.IRON).strength(5.0F, 6.0F).sounds(BlockSoundGroup.METAL));
-    public static final Block TIN_BLOCK = new MaterialBlock(FabricBlockSettings.of(Material.METAL, MaterialColor.IRON).strength(5.0F, 6.0F).sounds(BlockSoundGroup.METAL));
-    public static final Block ZINC_BLOCK = new MaterialBlock(FabricBlockSettings.of(Material.METAL, MaterialColor.IRON).strength(5.0F, 6.0F).sounds(BlockSoundGroup.METAL));
+    public static final Block ALUMINUM_BLOCK = new MaterialBlock(FabricBlockSettings.of(Material.METAL, MaterialColor.IRON).strength(5.0F, 6.0F).sounds(BlockSoundGroup.METAL).breakByTool(FabricToolTags.PICKAXES));
+    public static final Block COPPER_BLOCK = new MaterialBlock(FabricBlockSettings.of(Material.METAL, MaterialColor.IRON).strength(5.0F, 6.0F).sounds(BlockSoundGroup.METAL).breakByTool(FabricToolTags.PICKAXES));
+    public static final Block LEAD_BLOCK = new MaterialBlock(FabricBlockSettings.of(Material.METAL, MaterialColor.IRON).strength(5.0F, 6.0F).sounds(BlockSoundGroup.METAL).breakByTool(FabricToolTags.PICKAXES));
+    public static final Block NICKEL_BLOCK = new MaterialBlock(FabricBlockSettings.of(Material.METAL, MaterialColor.IRON).strength(5.0F, 6.0F).sounds(BlockSoundGroup.METAL).breakByTool(FabricToolTags.PICKAXES));
+    public static final Block PLATINUM_BLOCK = new MaterialBlock(FabricBlockSettings.of(Material.METAL, MaterialColor.IRON).strength(5.0F, 6.0F).sounds(BlockSoundGroup.METAL).breakByTool(FabricToolTags.PICKAXES));
+    public static final Block SILVER_BLOCK = new MaterialBlock(FabricBlockSettings.of(Material.METAL, MaterialColor.IRON).strength(5.0F, 6.0F).sounds(BlockSoundGroup.METAL).breakByTool(FabricToolTags.PICKAXES));
+    public static final Block TIN_BLOCK = new MaterialBlock(FabricBlockSettings.of(Material.METAL, MaterialColor.IRON).strength(5.0F, 6.0F).sounds(BlockSoundGroup.METAL).breakByTool(FabricToolTags.PICKAXES));
+    public static final Block ZINC_BLOCK = new MaterialBlock(FabricBlockSettings.of(Material.METAL, MaterialColor.IRON).strength(5.0F, 6.0F).sounds(BlockSoundGroup.METAL).breakByTool(FabricToolTags.PICKAXES));
 
     //alloy storage blocks
-    public static final Block BRASS_BLOCK = new MaterialBlock(FabricBlockSettings.of(Material.METAL, MaterialColor.IRON).strength(5.0F, 6.0F).sounds(BlockSoundGroup.METAL)); //zinc + copper or aluminum + copper
-    public static final Block BRONZE_BLOCK = new MaterialBlock(FabricBlockSettings.of(Material.METAL, MaterialColor.IRON).strength(5.0F, 6.0F).sounds(BlockSoundGroup.METAL));
-    public static final Block ELECTRUM_BLOCK = new MaterialBlock(FabricBlockSettings.of(Material.METAL, MaterialColor.IRON).strength(5.0F, 6.0F).sounds(BlockSoundGroup.METAL));
-    public static final Block INVAR_BLOCK = new MaterialBlock(FabricBlockSettings.of(Material.METAL, MaterialColor.IRON).strength(5.0F, 6.0F).sounds(BlockSoundGroup.METAL));
+    public static final Block BRASS_BLOCK = new MaterialBlock(FabricBlockSettings.of(Material.METAL, MaterialColor.IRON).strength(5.0F, 6.0F).sounds(BlockSoundGroup.METAL).breakByTool(FabricToolTags.PICKAXES)); //zinc + copper or aluminum + copper
+    public static final Block BRONZE_BLOCK = new MaterialBlock(FabricBlockSettings.of(Material.METAL, MaterialColor.IRON).strength(5.0F, 6.0F).sounds(BlockSoundGroup.METAL).breakByTool(FabricToolTags.PICKAXES));
+    public static final Block ELECTRUM_BLOCK = new MaterialBlock(FabricBlockSettings.of(Material.METAL, MaterialColor.IRON).strength(5.0F, 6.0F).sounds(BlockSoundGroup.METAL).breakByTool(FabricToolTags.PICKAXES));
+    public static final Block INVAR_BLOCK = new MaterialBlock(FabricBlockSettings.of(Material.METAL, MaterialColor.IRON).strength(5.0F, 6.0F).sounds(BlockSoundGroup.METAL).breakByTool(FabricToolTags.PICKAXES));
 }
